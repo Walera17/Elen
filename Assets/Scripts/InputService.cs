193ef50f82aeb6242f4a3456dc3262c30aad4294
DESKTOP-K3JDAAT\Walera <aluschta17@gmail.com>
2023-04-21 06:16:40 +0300

    Full Project

---
+++ Assets/Scripts/InputService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class InputService : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    [SerializeField] private GameObject restartPanel;
+    public event Action<Vector3> OnShoot;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (Physics.Raycast(Camera.main!.ScreenPointToRay(eventData.position), out RaycastHit hitInfo))
+            OnShoot?.Invoke(hitInfo.point);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+    }
+
+    public void ShowRestartPanel()
+    {
+        restartPanel.SetActive(true);
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
